190fd45ffdc03ff5ffbdd37fb9474736b03c4594
Nill-Kigers <90038624+Nill-Kigers@users.noreply.github.com>
2022-03-29 14:59:00 +0300

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static double MinAVG(string[] Marks)
+        {
+            double result = 0;
+            int[] array = new int[Marks.Length];
+            int[] Num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            double sum = 0;
+            for (int i = 0; i < Marks.Length; i++)
+            {
+                int num;
+                bool isNum = int.TryParse(Marks[i], out num);
+                if (isNum)
+                {
+                    array[i] = Convert.ToInt32(Marks[i]);
+                    Console.WriteLine(array[i]);
+                    sum += array[i];
+                }
+                else 
+                {
+                    result = 0;
+                }
+            }
+            if (Marks.Length == 0)
+            {
+                result = 0;
+            }
+            else
+            {
+                result = sum / Marks.Length;
+            }
+            return result;
+
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1Tests/Class1Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void MinAVGTest()
+        {
+            string[] pass = { "1", "2" };
+            double expected = 1.5;
+            double actual = Class1.MinAVG(pass);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void LetterAndNum()
+        {
+            string[] pass = { "f", "2" };
+            double expected = 1;
+            double actual = Class1.MinAVG(pass);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Div0()
+        {
+            string[] pass = {};
+            double expected = 0;
+            double actual = Class1.MinAVG(pass);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
